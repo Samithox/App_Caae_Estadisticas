f5adf1adf6e286e5f96f6f488fc21309c8c7698d
Samithox <samuel.sp10@gmail.com>
2018-06-01 19:29:53 -0400

    Cambios login

--- Caae_Estadisticas/urls.py
+++ Caae_Estadisticas/urls.py
@@ -1,11 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from django.contrib.auth import views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Estadisticas.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout'),
+    #url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'', include('app.urls')),
+
 )
--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
-from . import views
+from . import views 
+from django.contrib.auth.views import login
 
 urlpatterns = [
-    url(r'^$', views.login),
+    url(r'^$', views.home),
+    #url(r'^logout/$', views.logout,{'next_page':'accounts:login'}),
     ]
