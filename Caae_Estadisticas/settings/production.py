d46c2fc01e14db1538e36263d9259ea7d1b906dd
Samithox <samuel.sp10@gmail.com>
2018-07-12 12:27:33 -0400

    Update Site

--- Caae_Estadisticas/settings/production.py
+++ Caae_Estadisticas/settings/production.py
@@ -1,18 +1,21 @@
+from Caae_Estadisticas.settings.base import base
 import os
+ 
+
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'NAME': 'caae',                      # Or path to database file if using sqlite3.
+        'USER': 'caae',                      # Not used with sqlite3.
+        'PASSWORD': '@CAAE.2018',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
 COMPRESS_OFFLINE = True
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['localhost','172.0.0.0']
