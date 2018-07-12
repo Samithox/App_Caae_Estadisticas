bd9840d8f9fc13db04840e0ed71955cc191f3aa7
Samithox <samuel.sp10@gmail.com>
2018-07-12 12:09:01 -0400

    Update Site

--- Caae_Estadisticas/settings/base.py
+++ Caae_Estadisticas/settings/base.py
@@ -72,9 +72,23 @@ USE_L10N = True
 
 USE_TZ = True
 
-TEMPLATE_DIRS = (
-    join(PROJECT_ROOT, 'templates'),
-    )
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            join(PROJECT_ROOT, 'templates')
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 
 STATIC_URL = '/static/'
